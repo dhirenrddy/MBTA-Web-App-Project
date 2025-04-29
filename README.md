0b0d605e8ad9a7d2a14a7457fe732c90c50721f6
Dhiren Reddy <dreddy1@babson.edu>
2025-04-28 22:14:53 -0400

    final push for assignment 3

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MBTA-Web-App-Project
  This is the repository for the MBTA project (Assginment 3)
+
+# Project Overview 
+
+The main goal of the assignment prompt is to input a location in the Massachussetts area, where in the code will return the nearest MBTA train stop location. Furthermore, an additional feature where in wheelchair accessability was incoporated to ensure disabled passengers are aware of the disability features for the nearest MBTA station. The goal of this assignment was to incorporate API keys (MBTA key and MAPBOX token), flask, html as well as python to ensure a smooth experience could be used by the user. Overall, this project not only showcased the importance of incorporating different languages and applications, but it highlighted how to transition between these elements to augment the user experience. 
+
+# Project Reflection
+
+One of the key difficulties I experienced during the coding process was the ability to utilize my API keys and incorporating them in the code. My first obstacle was retrieving the key itself as a time limit was required for a key to be generated. This was one of the main reasons as to why my submission was delayed. Additionally, although I used and mimic the resources discussed in class, it was evident that the use of AI was intstrumental to ensure that the code was functioning. Thus, it is apparent that although the language used was somewhat similar to AI generated code, most of it was generated during the debugging process as my in class knowledgeded was insuffucient. 
+
+One of the more enjoyable aspects of this project was the development of the html pages required for the user experience. As I have undertaken OIm 3690 (Web Technologies), I enjoyed using this knowledge to guide me and aid me in the development of my web pages. 
+
+Lastly, it was noticable that via this experience, I was able to understand how to utilize AI adequately for my future endeavours. Rather than relying on AI for code generation, I became aware of Ai's uniqe ability to decode and debug my code thoroughly and point out my mistakes. This ensured that not only was my time efficiently used, but my errors could bve averted during my future endeavours as well. 
--- mbta_helper.py
+++ mbta_helper.py
@@ -7,47 +7,44 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MAPBOX_TOKEN = os.getenv("MAPBOX_TOKEN")
-MBTA_API_Key = os.getenv("MBTA_API_KEY")
+MBTA_API_KEY = os.getenv("MBTA_API_KEY")  # FIXED capitalization
 
 MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 def get_coords(place_name: str) -> tuple:
-    """If the user inputs a location (i.e. name), they receive the coordoinates for their location"""
+    """Given a place name, return (latitude, longitude) as floats."""
     query = urllib.parse.quote(place_name)
-    url = f"{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}&limit=1" # Used ChatGPT for this line as code was not working during the debugging process
+    url = f"{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}&limit=1"
 
-    with urllib.request.urlopen(url) as reponse:
+    with urllib.request.urlopen(url) as response:  # FIXED typo
         text_response = response.read().decode('utf-8')
         data = json.loads(text_response)
     
     coordinates = data["features"][0]["geometry"]["coordinates"]
     longitude, latitude = coordinates
-    return coordinates
+    return latitude, longitude  # FIXED order: lat first!
 
 def closest_station(lat: float, lng: float) -> tuple:
-    """Given user's coordinates, return 'station_name' and if station is wheelchair accessible"""
-    
+    """Given user's coordinates, return (station_name, wheelchair_accessibility)."""
     url = (f"{MBTA_BASE_URL}?api_key={MBTA_API_KEY}"
-       f"&filter[latitude]={latitude}&filter[longitude]={longitude}"
-       f"&sort=distance")
-    
+           f"&filter[latitude]={lat}&filter[longitude]={lng}"
+           f"&sort=distance")
+
     with urllib.request.urlopen(url) as response:
         response_text = response.read().decode('utf-8')
         data = json.loads(response_text)
     
     stop = data["data"][0]["attributes"]
     station_name = stop["name"]
-    wheelchair_accessiblity = stop["wheelchair_boarding"]
-    return station_name, wheelchair_accessiblity
+    wheelchair_accessibility = stop["wheelchair_boarding"]  # FIXED spelling
+    return station_name, wheelchair_accessibility
 
-    
-    
 def nearest_stop(place_name: str) -> tuple:
-    """Find the nearest MBTA station based of the user's coordinates"""
+    """Find the nearest MBTA station based on a place name."""
     latitude, longitude = get_coords(place_name)
-    station_name, wheelchair_accessiblity = closest_station(latitude, longitude)
-    return station_name, wheelchair_accessiblity
+    station_name, wheelchair_accessibility = closest_station(latitude, longitude)
+    return station_name, wheelchair_accessibility
 
 if __name__ == "__main__":
     print(nearest_stop("Beacon Hill"))
